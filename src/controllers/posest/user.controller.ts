e0070a150d02d333f90dce54f48805f637f1d4b6
matheuslima-pwr <matheuslima@pwrgestao.com>
2024-09-30 16:03:37 -0300

    type correction

--- src/controllers/posest/user.controller.ts
+++ src/controllers/posest/user.controller.ts
@@ -1,9 +1,26 @@
 import { findAll } from "@/services/posest/user.service";
 import { NextResponse } from "next/server";
 
+type User = {
+    id: number;
+    email: string;
+    nome: string;
+    telefone: string;
+    empresa: string;
+    segmento: string;
+    faturamento: number; // Update the type to number
+    respostas: {
+        pergunta_id: number;
+        pontuacao: number;
+    }[];
+    operacional: string;
+    cliente: string;
+    produto: string;
+}
+
 export async function getUsers() {
     try {
-        const users: any = await findAll();
+        const users: User[] = await findAll();
         return NextResponse.json(users, { status: 200 }); // OK
     } catch (error) {
         return NextResponse.json({ error: 'Erro ao buscar respostas' }, { status: 500 }); // Internal Server Error
